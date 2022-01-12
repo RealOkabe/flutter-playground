05739c8476f2ff795cd5ee587e875caffe83c74e
RealOkabe <anmolzagrawal@gmail.com>
2022-01-13 00:39:27 +0530

    Added comments to explain the program

--- default_app/lib/main.dart
+++ default_app/lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+// Everything in flutter is a widget
+// main is the starting point of the application
+// runApp is the function that the main function is supposed to run. It is the default one
+// const MaterialApp is preferred as flutter recommends making constructors constant
+// textAlign is a property of the Text widget which is used to align the text
+// By default textAlign is set to TextAlign.left
+
 void main() =>
   runApp(
     const MaterialApp(
