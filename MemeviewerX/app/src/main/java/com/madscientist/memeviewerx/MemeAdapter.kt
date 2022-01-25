f16d8c2ab57efb840045543c99d2b7b54cd34027
RealOkabe <anmolzagrawal@gmail.com>
2022-01-25 13:33:19 +0530

    Added the Memeadapter along with comments
    This is the adapter needed to process data for RecyclerView

--- MemeviewerX/app/src/main/java/com/madscientist/memeviewerx/MemeAdapter.kt
+++ MemeviewerX/app/src/main/java/com/madscientist/memeviewerx/MemeAdapter.kt
@@ -1,23 +1,38 @@
 package com.madscientist.memeviewerx
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MemeAdapter: RecyclerView.Adapter<MemeAdapter.MemeViewHolder>() {
+// A recycler view needs an adapter which can provide the data that needs to be shown in the view
+// The adapter should have a viewholder that holds the view that is to be shown in the recycler view
+
+class MemeAdapter(val memeList: List<Meme>): RecyclerView.Adapter<MemeAdapter.MemeViewHolder>() {
+    // The viewholder holds the contents of the layout
     class MemeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
+        val memeName = itemView.findViewById<TextView>(R.id.memeText)
+        val memeImage = itemView.findViewById<ImageView>(R.id.memeImage)
+
     }
 
+    // This creates the view holder and converts the xml layout to java
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemeViewHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.layout_meme, parent, false)
+        return MemeViewHolder(view)
     }
 
+    // This binds actual data to the layout
     override fun onBindViewHolder(holder: MemeViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.memeName.text = memeList[position].memeName
+        holder.memeImage.setImageBitmap(memeList[position].memeImage)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return memeList.size
     }
 }
