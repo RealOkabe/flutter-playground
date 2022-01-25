5ef12ced08e8b829ce7a1451d04881be8b4a6723
RealOkabe <anmolzagrawal@gmail.com>
2022-01-25 12:19:39 +0530

    Added code and permission to fetch Memes from internet

--- MemeviewerX/app/src/main/java/com/madscientist/memeviewerx/MainActivity.kt
+++ MemeviewerX/app/src/main/java/com/madscientist/memeviewerx/MainActivity.kt
@@ -2,10 +2,37 @@ package com.madscientist.memeviewerx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.madscientist.memeviewerx.util.Constants
+import okhttp3.*
+import java.io.IOException
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        try {
+            fetchMemes()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
+
+    fun fetchMemes() {
+        val request = Request.Builder().url(Constants.MEME_URL).build()
+        val call = OkHttpClient().newCall(request)
+        call.enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body()
+                Log.d("*****", body!!.string())
+            }
+
+        })
+    }
+
 }
