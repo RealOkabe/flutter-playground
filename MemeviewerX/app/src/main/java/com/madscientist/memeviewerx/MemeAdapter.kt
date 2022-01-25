bfe95973a38ca6053680cb9b9097c9e0ae3ba5d0
RealOkabe <anmolzagrawal@gmail.com>
2022-01-25 11:35:00 +0530

    Added adapter for recyclerview and okhttp dependency

---
+++ MemeviewerX/app/src/main/java/com/madscientist/memeviewerx/MemeAdapter.kt
@@ -0,0 +1,23 @@
+package com.madscientist.memeviewerx
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MemeAdapter: RecyclerView.Adapter<MemeAdapter.MemeViewHolder>() {
+    class MemeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemeViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MemeViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
